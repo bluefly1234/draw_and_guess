3ee146aaa4e2bdb41f7ff030442c7bc8970cfb8f
pzli <446542051@qq.com>
2017-04-19 18:45:02 +0800

    updata DataService.js

--- server/service/DataService.js
+++ server/service/DataService.js
@@ -1,25 +1,31 @@
 import fs from 'fs'
 import path from 'path'
 
+// 获得一个乱序数组
 function getRandomArray(array) {
   if (array) {
-    return array.sort(_ => {
-      return (parseInt(Math.random() * 1000) % 2) ? 1 : -1
+    return array.sort((a, b) => {
+      return Math.random() > 0.5 ? 1 : -1
     })
   }
   return array
 }
 
+// 项目根路径
 const projectRoot = path.resolve(__dirname, '../')
+// 同步读取题目文件和昵称文件，由于是本地静态文件所以可以用同步，如果从数据库中获取最好是用异步方法
 const wordData = fs.readFileSync(projectRoot + '/word.txt','utf-8') // 题目文件
 const nickNameData = fs.readFileSync(projectRoot + '/nickname.txt', 'utf-8') // 昵称文件
+
 const allWord = wordData.split('\n')
 const allNameArray = nickNameData.split('\n')
+
 let keyIndex = 0
 let nameIndex = 0
 
-let allKeys = allWord.map(w => {
-  return w.split(':')
+// 保温杯:日用品
+let allKeys = allWord.map(item => {
+  return item.split(':')
 })
 
 let allNames = getRandomArray(allNameArray)
@@ -29,6 +35,7 @@ allKeys = getRandomArray(allKeys)
 console.log('projectRoot:', projectRoot)
 console.log('allGameKeyLength: ', allKeys.length)
 console.log('allNameLength: ', allNames.length)
+
 export default {
   getNextKey () {
     let keyWord = allKeys[keyIndex++]
--- server/service/GlobalEmitService.js
+++ server/service/GlobalEmitService.js
@@ -1,13 +1,17 @@
+// events是所有事件的数组集合，每一项是类type的数组
 const events = {}
 
+// 监听事件
 global.$on = (type, fn) => {
   if (typeof fn === 'function') {
+  	// 将同一type的事件push到同一个数组中
     const allFunction = events[type] || []
     allFunction.push(fn)
     events[type] = allFunction
   }
 }
 
+// 触发事件
 global.$emit = (type, data) => {
   const allFunction = events[type] || []
   allFunction.forEach(fn => fn(data))
